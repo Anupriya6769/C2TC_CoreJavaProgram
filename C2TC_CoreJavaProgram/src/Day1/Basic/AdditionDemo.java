3fe0ca1a50fb4bb6cd86b2c978be46e27e166739
Anupriya6769 <anupriya6769@gmail.com>
2025-07-21 15:28:46 +0530

    Addition program

---
+++ C2TC_CoreJavaProgram/src/Day1/Basic/AdditionDemo.java
@@ -0,0 +1,15 @@
+package Day1.Basic;
+import java.util.*;
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+       int a,b,c;
+       Scanner sc =new Scanner(System.in);
+       System.out.println("Enter the values:");
+       a=sc.nextInt();
+       b=sc.nextInt();
+       c=a+b;
+       System.out.println("Addition of two number="+c);
+	}
+
+}
--- C2TC_CoreJavaProgram/src/Day1/Basic/SampleDemo.java
+++ C2TC_CoreJavaProgram/src/Day1/Basic/SampleDemo.java
@@ -2,7 +2,6 @@ package Day1.Basic;
 public class SampleDemo {
 	public static void main(String[] args) {
 		System.out.println("Welcome to JFS");
-
 	}
 
 }
