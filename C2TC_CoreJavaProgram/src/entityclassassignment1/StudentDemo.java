db8271aa11fc4f4c4595b7b1ea21db1aaeb3753e
Anupriya6769 <anupriya6769@gmail.com>
2025-07-22 12:25:16 +0530

    This is my First Java Task.

---
+++ C2TC_CoreJavaProgram/src/entityclassassignment1/StudentDemo.java
@@ -0,0 +1,17 @@
+package entityclassassignment1;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Student s1 = new Student(101, "Alice", "Chennai");
+	        Student s2 = new Student(102, "Bob", "Coimbatore");
+	        Student s3 = new Student(103, "Charlie", "Madurai");
+	        System.out.println("Student Details:");
+	        s1.display();
+	        s2.display();
+	        s3.display();
+
+	}
+
+}
